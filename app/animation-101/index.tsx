7ca5da1391cf7c371b45ab7899d0bc29687cb2d7
Jeral Pineda <jeral.cris@gmail.com>
2024-11-07 21:21:10 -0600

    fix: Animaciones 101 - Basicas

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native';
+import { useRef } from "react";
+import { Animated } from "react-native";
+
+import { ThemedButton } from "@/presentation/shared/ThemedButton";
+import { ThemedView } from "@/presentation/shared/ThemedView";
 
 const Animation101Screen = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView margin className="flex-1 items-center justify-center">
+      <Animated.View
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl"
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpacity,
+        }}
+      />
+
+      <ThemedButton className="my-4" onPress={fadeIn}>
+        FadeIn
+      </ThemedButton>
+
+      <ThemedButton className="" onPress={fadeOut}>
+        FadeOut
+      </ThemedButton>
+    </ThemedView>
   );
 };
 export default Animation101Screen;
