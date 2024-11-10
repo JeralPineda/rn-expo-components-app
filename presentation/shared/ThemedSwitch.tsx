cccd517f228f15c702a51e678656883ad149e4bd
Jeral Pineda <jeral.cris@gmail.com>
2024-11-10 16:53:09 -0600

    fix: ending ThemedSwitch

--- presentation/shared/ThemedSwitch.tsx
+++ presentation/shared/ThemedSwitch.tsx
@@ -1,5 +1,7 @@
-import { Pressable, Switch } from "react-native";
+import { Platform, Pressable, Switch, View } from "react-native";
+
 import { ThemedText } from "./ThemedText";
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 interface ThemedSwitchProps {
   text?: string;
@@ -9,19 +11,33 @@ interface ThemedSwitchProps {
   onValueChange: (value: boolean) => void;
 }
 
+const isAndroid = Platform.OS === "android";
+
 export function ThemedSwitch({
   text,
   value,
   className,
   onValueChange,
 }: ThemedSwitchProps) {
+  const switchActiveColor = useThemeColor({}, "primary");
+
   return (
     <Pressable
-      className={`flex flex-row items-center justify-between active:opacity-80 ${className}`}
+      className={`flex flex-row items-center justify-between mx-2 active:opacity-80 ${className}`}
+      onPress={() => onValueChange(!value)}
     >
-      {text && <ThemedText type="h2">{text}</ThemedText>}
+      {text ? <ThemedText type="h2">{text}</ThemedText> : <View />}
 
-      <Switch value={value} onValueChange={onValueChange} />
+      <Switch
+        value={value}
+        onValueChange={onValueChange}
+        // ios_backgroundColor={value ? "green" : "red"}
+        thumbColor={isAndroid ? switchActiveColor : ""}
+        trackColor={{
+          false: "grey",
+          true: switchActiveColor,
+        }}
+      />
     </Pressable>
   );
 }
