2df928e6f24a38ef63146afa8fbb8acb2b0ea7b9
Jeral Pineda <jeral.cris@gmail.com>
2024-11-10 18:45:14 -0600

    fix: add ThemedTextInput

--- app/text-inputs/index.tsx
+++ app/text-inputs/index.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native';
+import { useState } from "react";
+import { Text } from "react-native";
+
+import { ThemedCard } from "@/presentation/shared/ThemedCard";
+import { ThemedTextInput } from "@/presentation/shared/ThemedTextInput";
+import { ThemedView } from "@/presentation/shared/ThemedView";
 
 const TextInputsScreen = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
   return (
-    <View>
-      <Text>TextInputsScreen</Text>
-    </View>
+    <ThemedView margin>
+      <ThemedCard className="my-5">
+        <ThemedTextInput
+          placeholder="Nombre completo"
+          autoCapitalize={"words"}
+          autoCorrect={false}
+          onChangeText={(text) => setForm({ ...form, name: text })}
+        />
+
+        <ThemedTextInput
+          placeholder="Email"
+          autoCapitalize="none"
+          keyboardType="email-address"
+          onChangeText={(text) => setForm({ ...form, email: text })}
+        />
+
+        <ThemedTextInput
+          placeholder="Phone"
+          keyboardType="phone-pad"
+          onChangeText={(text) => setForm({ ...form, phone: text })}
+        />
+      </ThemedCard>
+
+      <ThemedCard>
+        <Text className="text-black dark:text-white p-3">
+          {JSON.stringify(form, null, 2)}
+        </Text>
+      </ThemedCard>
+    </ThemedView>
   );
 };
 export default TextInputsScreen;
---
+++ presentation/shared/ThemedTextInput.tsx
@@ -0,0 +1,15 @@
+import { TextInput, TextInputProps } from "react-native";
+
+interface ThemedTextInputProps extends TextInputProps {
+  className?: string;
+}
+
+export function ThemedTextInput({ className, ...rest }: ThemedTextInputProps) {
+  return (
+    <TextInput
+      {...rest}
+      className={`p-3 mb-4 border border-slate-200 dark:border-slate-700 text-black dark:text-white focus:border-slate-400/50 focus:dark:border-slate-400/50 rounded-md ${className}`}
+      placeholderTextColor="gray"
+    />
+  );
+}
