91b62689cf364e32480b03f57c47e44d68aafee8
Jeral Pineda <jeral.cris@gmail.com>
2024-11-11 20:57:02 -0600

    fix: Pull to refrech

--- app/pull-to-refresh/index.tsx
+++ app/pull-to-refresh/index.tsx
@@ -1,10 +1,44 @@
-import { View, Text } from 'react-native';
+import { useState } from "react";
+import { RefreshControl, ScrollView } from "react-native";
+
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { ThemedText } from "@/presentation/shared/ThemedText";
+import { ThemedView } from "@/presentation/shared/ThemedView";
 
 const PullToRefreshScreen = () => {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const primaryColor = useThemeColor({}, "primary");
+  const backgroundColor = useThemeColor(
+    {
+      dark: "black",
+      light: "white",
+    },
+    "background",
+  );
+
+  const onRefresh = () => {
+    setIsRefreshing(true);
+
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 2000);
+  };
+
   return (
-    <View>
-      <Text>PullToRefreshScreen</Text>
-    </View>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+          colors={[primaryColor, "red", "orange", "green"]}
+          // progressBackgroundColor={backgroundColor}
+        />
+      }
+    >
+      <ThemedView margin>
+        <ThemedText>PullToRefreshScreen</ThemedText>
+      </ThemedView>
+    </ScrollView>
   );
 };
 export default PullToRefreshScreen;
